7e4ec80d8648f94c39d868b04e2a76d42df5ec54
Autumn <adamhoffensetz@gmail.com>
2025-11-12 16:58:56 -0500

    Fix file name prefix issue

--- Project 1 Image Analysis.py
+++ Project 1 Image Analysis.py
@@ -6,12 +6,8 @@ from tqdm import tqdm
 
 #%% Creating the master bias
 
-# ISSUES:
-#   Different file name formats
-#   Different filters
-
 # Median combine the bias images
-def create_master_bias(image_folder, num_images):
+def create_master_bias(image_folder, num_images, filter_name, file_prefix=""):
     # Load the images
     path = f"{image_folder}/BIAS"
     g_bias = [] # this creates an unfilled list
@@ -20,7 +16,7 @@ def create_master_bias(image_folder, num_images):
         number = str(i)
         while len(number) < 4:
             number = f"0{number}" # this is creating an index for numbers 0000 through num_images to call
-        g_bias.append([np.array(fits.open(f"{path}/{number}-g'.fits")[0].data)])
+        g_bias.append([np.array(fits.open(f"{path}/{file_prefix}{number}-{filter_name}.fits")[0].data)])
     print('Created a list containing each image')
 
     g_bias = np.vstack(g_bias)
@@ -31,7 +27,7 @@ def create_master_bias(image_folder, num_images):
     print('Transposed the matrix')
     
     # Take the median of each pixel value
-    for i in tqdm(range(len(g_bias)), desc ='Rows', unit = 'row'):
+    for i in tqdm(range(len(g_bias)), desc =f"{image_folder} Median Combination", unit = 'row'):
         for j in range(len(g_bias[i])):
             g_bias[i, j] = np.median(g_bias[i, j])
 
@@ -41,11 +37,11 @@ def create_master_bias(image_folder, num_images):
 
     # Save the combined FITS bias image
     hdu = fits.PrimaryHDU(mast_bias)
-    hdu.writeto(f"{path}/mast_bias.fits", overwrite = True)
+    hdu.writeto(f"{path}/mast_bias-{filter}.fits", overwrite = True)
     print('Saved the .fits image')
 
-# Create master bias for 9/08
-create_master_bias("20250908_07in_NGC6946")
-
-# Create master bias for 9/28
-create_master_bias("20250928_07in_NGC6946")
+# Create master biases
+create_master_bias("20250908_07in_NGC6946", 12, "g'")
+create_master_bias("20250928_07in_NGC6946", 7, "ha", "NGC6946_")
+create_master_bias("20251009_07in_NGC6946", 7, "ha", "BIAS_NGC6946_")
+create_master_bias("20251015_07in_NGC6946", 7, "g'", "BIAS_NGC6946_")
