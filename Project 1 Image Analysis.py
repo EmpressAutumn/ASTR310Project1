097d9c1ee48118e6995006f48528e7458f4524bf
Autumn <adamhoffensetz@gmail.com>
2025-11-12 15:58:49 -0500

    Generalize master bias code

--- Project 1 Image Analysis.py
+++ Project 1 Image Analysis.py
@@ -7,76 +7,41 @@ from tqdm import tqdm
 #%% Creating the master bias for 9/08
 
 # Median combine the bias images
-def create_master_bias():
+def create_master_bias(image_folder):
     # Load the images
-    path = "20250908_07in_NGC6946/BIAS"
-    g_bias_908 = [] #this creates an unfilled list
+    path = f"{image_folder}/BIAS"
+    g_bias = [] # this creates an unfilled list
     
     for i in range(12):
         number = str(i)
         while len(number) < 4:
             number = f"0{number}" #this is creating an index for numbers 0000 through 0007 to call
-        g_bias_908.append([np.array(fits.open(f"{path}/{number}-g'.fits")[0].data)])
+        g_bias.append([np.array(fits.open(f"{path}/{number}-g'.fits")[0].data)])
     print('Created a list containing each image')
 
-    g_bias_908 = np.vstack(g_bias_908)
+    g_bias = np.vstack(g_bias)
     print('Stacked each image matrix')
 
     # Transpose the matrix: image[row[column[]]] -> row[column[image[]]]
-    g_bias_908 = np.transpose(g_bias_908, (1, 2, 0))
+    g_bias = np.transpose(g_bias, (1, 2, 0))
     print('Transposed the matrix')
     
     # Take the median of each pixel value
-    for i in tqdm(range(len(g_bias_908)), desc ='Rows', unit = 'row'):
-        for j in range(len(g_bias_908[i])):
-            g_bias_908[i, j] = np.median(g_bias_908[i, j])
+    for i in tqdm(range(len(g_bias)), desc ='Rows', unit = 'row'):
+        for j in range(len(g_bias[i])):
+            g_bias[i, j] = np.median(g_bias[i, j])
 
     # Remove the duplicate median values (Thank you NumPy for being awful!)
-    mast_bias_908 = g_bias_908[:, :, 0]
+    mast_bias = g_bias[:, :, 0]
     print('Removed duplicate median values')
 
     # Save the combined FITS bias image
-    hdu = fits.PrimaryHDU(mast_bias_908)
-    hdu.writeto(f"{path}/mast_bias_908.fits", overwrite = True)
+    hdu = fits.PrimaryHDU(mast_bias)
+    hdu.writeto(f"{path}/mast_bias.fits", overwrite = True)
     print('Saved the .fits image')
 
-create_master_bias()
+# Create master bias for 9/08
+create_master_bias("20250908_07in_NGC6946")
 
-#%% Creating the Biases for 9/28
-
-# Median combine the bias images
-def create_master_bias():
-    # Load the images
-    path = "20250928_07in_NGC6946/BIAS"
-    Ha_bias_928 = [] #this creates an unfilled list
-    
-    for i in range(7):
-        number = str(i)
-        while len(number) < 4:
-            number = f"0{number}" #this is creating an index for numbers 0000 through 0007 to call
-        Ha_bias_928.append([np.array(fits.open(f"{path}/NGC6946_{number}-ha.fits")[0].data)])
-    print('Created a list containing each image')
-
-    Ha_bias_928 = np.vstack(Ha_bias_928)
-    print('Stacked each image matrix')
-
-    # Transpose the matrix: image[row[column[]]] -> row[column[image[]]]
-    Ha_bias_928 = np.transpose(Ha_bias_928, (1, 2, 0))
-    print('Transposed the matrix')
-    
-    # Take the median of each pixel value
-    for i in tqdm(range(len(Ha_bias_928)), desc ='Rows', unit = 'row'):
-        for j in range(len(Ha_bias_928[i])):
-            Ha_bias_928[i, j] = np.median(Ha_bias_928[i, j])
-
-    # Remove the duplicate median values (Thank you NumPy for being awful!)
-    mast_bias_928 = Ha_bias_928[:, :, 0]
-    print('Removed duplicate median values')
-
-    # Save the combined FITS bias image
-    hdu = fits.PrimaryHDU(mast_bias_928)
-    hdu.writeto(f"{path}/mast_bias_928.fits", overwrite = True)
-    print('Saved the .fits image')
-
-create_master_bias()
-
+# Create master bias for 9/28
+create_master_bias("20250928_07in_NGC6946")
