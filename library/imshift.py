8fe5b9db98a2d71649548c4ca6f7f8170c1bb83e
Autumn <adamhoffensetz@gmail.com>
2025-11-21 20:00:45 -0500

    Add image rotation function to imshift

--- library/imshift.py
+++ library/imshift.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def imshift(im,nr,nc):
+def imshift(im,nr,nc, rotate=False):
     """Shifts an image by nr rows and nc columns
     (which can be either positive or negative)"""
 
@@ -15,4 +15,13 @@ def imshift(im,nr,nc):
     c1=max(0,nc)
     c2=min(b,nc+b)
     imr[r1:r2, c1:c2] = im[ir1:ir2, it1:it2]
+
+    # Image rotation ability by Autumn
+    if rotate:
+        out = np.zeros(imr.shape)
+        for i in range(imr.shape[0]):
+            for j in range(imr.shape[1]):
+                out[i, imr.shape[1] - 1 - j] = imr[imr.shape[0] - 1 - i, j]
+        return out
+
     return imr
